957a12c19b3395f3b9717d33ee2be8098c68b615
Arnau Siches <asiches@gmail.com>
2017-12-02 16:00:30 +0000

    elixir: Add Day2 part 2
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- ex/lib/day2.ex
+++ ex/lib/day2.ex
@@ -2,13 +2,24 @@ defmodule Aoc.Day2 do
   @doc """
   http://adventofcode.com/2017/day/2
 
-    iex> Aoc.Day2.solve(["5\t1\t9\t5"],["7\t5\t3"],["2\t4\t6\t8"])
+    iex> Aoc.Day2.solve(["5\t1\t9\t5","7\t5\t3","2\t4\t6\t8"])
     18
   """
   def solve(rows) do
     rows
     |> Stream.map(&parse_row/1)
     |> Stream.map(&row_diff/1)
+    |> Enum.sum()
+  end
+
+  @doc """
+    iex> Aoc.Day2.solve_b(["5\t9\t2\t8", "9\t4\t7\t3", "3\t8\t6\t5"])
+    9
+  """
+  def solve_b(rows) do
+    rows
+    |> Stream.map(&parse_row/1)
+    |> Stream.map(&row_div/1)
     |> Enum.sum()
   end
 
@@ -36,5 +47,52 @@ defmodule Aoc.Day2 do
     {x, y} = Enum.min_max(xs)
 
     y - x
+  end
+
+  @doc """
+    Could be done with:
+
+      xsi = Enum.with_index(xs)
+      (for {x, i} <- xsi, {y, j} <- xsi, i != j && rem(x, y) == 0, do: div(x, y))
+      |> List.first
+
+    But `Enum.reduce_while/2` seems more fun.
+
+    iex> Aoc.Day2.row_div([5, 9, 2, 8])
+    4
+
+    iex> Aoc.Day2.row_div([9, 4, 7, 3])
+    3
+
+    iex> Aoc.Day2.row_div([3, 8, 6, 5])
+    2
+  """
+  def row_div(xs) do
+    xsi = Enum.with_index(xs)
+
+    xsi
+    |> Enum.reduce_while(0, fn (xi, acc) ->
+      case divide(xi, xsi) do
+        {:ok, result} -> {:halt, result}
+        _ -> {:cont, acc}
+      end
+    end)
+  end
+
+  @doc """
+    iex> Aoc.Day2.divide({5, 0}, Enum.with_index([5, 9, 2, 8]))
+    :not_found
+
+    iex> Aoc.Day2.divide({8, 3}, Enum.with_index([5, 9, 2, 8]))
+    {:ok, 4}
+  """
+  def divide({x, i}, xsi) do
+    Enum.reduce_while(xsi, :not_found, fn ({y, j}, acc) ->
+      if i != j && rem(x, y) == 0 do
+        {:halt, {:ok, div(x, y)}}
+      else
+        {:cont, acc}
+      end
+    end)
   end
 end
--- ex/test/ex_test.exs
+++ ex/test/ex_test.exs
@@ -2,10 +2,18 @@ defmodule ExTest do
   use ExUnit.Case
 
   doctest Aoc.Day1
+  doctest Aoc.Day2
 
-  test "day2" do
+  test "day2 part 1" do
     input = File.stream!("./data/day2.txt")
 
     assert Aoc.Day2.solve(input) == 47136
   end
+
+  test "day2 part 2" do
+    input = File.stream!("./data/day2.txt")
+
+    assert Aoc.Day2.solve_b(input) == 250
+  end
+
 end

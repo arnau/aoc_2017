df1850e2bce0e9b0d05348a25e8fd50322d09da4
Arnau Siches <asiches@gmail.com>
2017-12-05 07:47:11 +0000

    elixir: Day 3 part 2
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- ex/lib/day3.ex
+++ ex/lib/day3.ex
@@ -24,163 +24,157 @@ defmodule Aoc.Day3 do
     iex> Aoc.Day3.solve(312051)
     430
   """
+  def solve(1), do: 0
   def solve(x) do
-    distance(x)
+    {x, y} = coord_stream() |> Stream.drop(x - 1) |> Enum.take(1) |> List.first()
+
+    abs(y) + abs(x)
   end
 
   @doc """
-    iex> Aoc.Day3.distance(1)
-    0
-
-    iex> Aoc.Day3.distance(12)
-    3
-  """
-  def distance(x) do
-    cardinal_distance(x) + center_distance(x)
-  end
-
-  @doc """
-  Rings:
-
-  1    1
-  2    9
-  3    25
-  4    49
-  n    m = (2n - 1)^2
-       sqrt(m) = 2n - 1
-       (sqrt(m) + 1) / 2 = n
-
-    iex> Aoc.Day3.ring(1)
-    1
-
-    iex> Aoc.Day3.ring(9)
-    2
-
-    iex> Aoc.Day3.ring(25)
-    3
-
-    iex> Aoc.Day3.ring(49)
-    4
-
-    iex> Aoc.Day3.ring(12)
-    3
-
-    iex> Aoc.Day3.ring(2)
-    2
-
-    iex> Aoc.Day3.ring(80)
+    iex> Aoc.Day3.solve_b(4)
     5
+
+    iex> Aoc.Day3.solve_b(747)
+    806
+
+    iex> Aoc.Day3.solve_b(312051)
+    312453
   """
-  def ring(x) do
-    ((:math.sqrt(x) + 1) / 2)
-    |> Float.ceil()
-    |> trunc()
+  def solve_b(x) when x > 0 do
+    {_, res} = grid(x) |> List.first()
+
+    res
   end
 
   @doc """
-    iex> Aoc.Day3.cardinals(1)
-    [1, 1, 1, 1]
+    iex> Aoc.Day3.grid(1)
+    [{{1, 1}, 2},{{1, 0}, 1},{{0, 0}, 1}]
 
-    iex> Aoc.Day3.cardinals(2)
-    [2, 4, 6, 8]
+    iex> Aoc.Day3.grid(2)
+    [{{0, 1}, 4},{{1, 1}, 2},{{1, 0}, 1},{{0, 0}, 1}]
 
-    iex> Aoc.Day3.cardinals(3)
-    [11, 15, 19, 23]
+    iex> Aoc.Day3.grid(4) |> List.first()
+    {{-1, 1}, 5}
+
+    iex> Aoc.Day3.grid(11) |> List.first()
+    {{0, -1}, 23}
   """
-  def cardinals(ring) do
-    [east(ring), north(ring), west(ring), south(ring)]
+  def grid(x) when x > 0 do
+    coord_stream()
+    |> Enum.reduce_while([], fn (coord, acc) ->
+      prev = with {_, n} <- List.first(acc), do: n, else: (nil -> 0)
+
+      curr = Enum.sum(neighbours(coord, acc))
+
+      if prev <= x do
+        {:cont, [{coord, curr} | acc]}
+      else
+        {:halt, acc}
+      end
+    end)
   end
 
   @doc """
-    iex> Aoc.Day3.east(1)
-    1
+    iex> Aoc.Day3.neighbours({0, 0}, [])
+    [1]
 
-    iex> Aoc.Day3.east(2)
-    2
+    iex> Aoc.Day3.neighbours({1, 0}, [{{0, 0}, 1}])
+    [1]
 
-    iex> Aoc.Day3.east(3)
-    11
+    iex> Aoc.Day3.neighbours({-1, 0}, [{{0, 0}, 1},{{1, 0}, 1},{{1, 1}, 2},{{0, 1}, 4},{{-1, 1}, 5}])
+    [1, 4, 5]
   """
-  def east(ring), do:
-    4 * :math.pow(ring, 2) - 11 * ring + 8 |> trunc()
+  def neighbours({0, 0}, _), do: [1]
+  def neighbours({x, y}, xs) do
+    Enum.flat_map(xs, fn ({coord, n}) ->
+      is_neighbour = surroundings()
+                     |> Enum.map(fn ({x_s, y_s}) -> {x + x_s, y + y_s} == coord end)
+                     |> Enum.any?()
 
-  @doc """
-    iex> Aoc.Day3.north(1)
-    1
+      if is_neighbour, do: [n], else: []
+    end)
+  end
 
-    iex> Aoc.Day3.north(2)
-    4
-
-    iex> Aoc.Day3.north(3)
-    15
-  """
-  def north(ring) do
-    4 * :math.pow(ring, 2) - 9  * ring + 6 |> trunc()
+  def surroundings() do
+    [{-1, +1}, {0, +1}, {+1, +1},
+     {-1,  0},          {+1,  0},
+     {-1, -1}, {0, -1}, {+1, -1}]
   end
 
   @doc """
-    iex> Aoc.Day3.west(1)
-    1
+    iex> Aoc.Day3.next_coord(%{x: 0, y: 0, op: 1, count: 0, limit: 2})
+    %{x: 1, y: 0, op: 1, count: 1, limit: 2}
 
-    iex> Aoc.Day3.west(2)
-    6
+    iex> Aoc.Day3.next_coord(%{x: 1, y: 0, op: 1, count: 1, limit: 2})
+    %{x: 1, y: 1, op: -1, count: 0, limit: 4}
 
-    iex> Aoc.Day3.west(3)
-    19
+    iex> Aoc.Day3.next_coord(%{x: 0, y: 1, op: -1, count: 1, limit: 4})
+    %{x: -1, y: 1, op: -1, count: 2, limit: 4}
+
+    iex> Aoc.Day3.next_coord(%{x: -1, y: 1, op: -1, count: 2, limit: 4})
+    %{x: -1, y: 0, op: -1, count: 3, limit: 4}
   """
-  def west(ring) do
-    4 * :math.pow(ring, 2) - 7  * ring + 4 |> trunc()
+  def next_coord(state = %{x: x, op: op, count: count, limit: limit}) when count < (limit / 2) do
+    state
+    |> Map.put(:x, x + op)
+    |> Map.put(:count, count + 1)
+  end
+  def next_coord(state = %{y: y, op: op, count: count, limit: limit}) do
+    state = state
+    |> Map.put(:y, y + op)
+    |> Map.put(:count, count + 1)
+
+    if state[:count] == limit do
+      state
+      |> Map.put(:op, op * -1)
+      |> Map.put(:limit, limit + 2)
+      |> Map.put(:count, 0)
+    else
+      state
+    end
   end
 
   @doc """
-    iex> Aoc.Day3.south(1)
-    1
+  17  16  15  14  13
+  18   5   4   3  12
+  19   6   1   2  11
+  20   7   8   9  10
+  21  22  23---> ...
 
-    iex> Aoc.Day3.south(2)
-    8
+  k = 1, x = 0, y = 0   op = (+1), s = 1, v = :x
+  inc x
+  k = 2, x = 1, y = 0
+  inc y
+  k = 3, x = 1, y = 1
+  dec x
+  k = 4, x = 0, y = 1
+  dec x
+  k = 5, x = -1, y = 1
+  dec y
+  k = 6, x = -1, y = 0
+  dec y
+  k = 7, x = -1, y = -1
 
-    iex> Aoc.Day3.south(3)
-    23
+    iex> Aoc.Day3.coord_stream() |> Enum.take(1)
+    [{0, 0}]
+
+    iex> Aoc.Day3.coord_stream() |> Enum.take(3)
+    [{0, 0},{1, 0},{1, 1}]
+
+    iex> Aoc.Day3.coord_stream() |> Stream.drop(4) |> Enum.take(1)
+    [{-1, 1}]
+
+    iex> Aoc.Day3.coord_stream() |> Stream.drop(11) |> Enum.take(1)
+    [{2, 1}]
   """
-  def south(ring) do
-    4 * :math.pow(ring, 2) - 5  * ring + 2 |> trunc()
+  def coord_stream() do
+    Stream.unfold(%{x: 0, y: 0, op: 1, count: 0, limit: 2}, &coord_gen/1)
   end
 
-  @doc """
-    Smallest distance between the input and a cardinal.
+  defp coord_gen(current = %{x: x, y: y}) do
+    next = next_coord(current)
 
-    iex> Aoc.Day3.cardinal_distance(3)
-    1
-
-    iex> Aoc.Day3.cardinal_distance(12)
-    1
-
-    iex> Aoc.Day3.cardinal_distance(12)
-    1
-
-    iex> Aoc.Day3.cardinal_distance(25)
-    2
-  """
-  def cardinal_distance(x) do
-    ring(x)
-    |> cardinals()
-    |> Enum.map(&abs(&1 - x))
-    |> Enum.min()
-  end
-
-  @doc """
-  Distance to the center of the spiral (smallest ring).
-
-    iex> Aoc.Day3.center_distance(1)
-    0
-
-    iex> Aoc.Day3.center_distance(2)
-    1
-
-    iex> Aoc.Day3.center_distance(12)
-    2
-  """
-  def center_distance(x) do
-    ring(x) - 1
+    {{x, y}, next}
   end
 end
